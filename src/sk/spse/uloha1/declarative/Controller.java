2bc07e131e1e79774e6777acbf7333011c5d1b56
xtimmieex <timus.hudakova@gmail.com>
2026-03-30 09:06:39 +0200

    Uloha1

--- src/sk/spse/uloha1/declarative/Controller.java
+++ src/sk/spse/uloha1/declarative/Controller.java
@@ -2,6 +2,7 @@ package sk.spse.uloha1.declarative;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
@@ -11,11 +12,35 @@ public class Controller {
     private int counter = 0;
 
     @FXML
-    private TextField counterField;
+    private TextField cField;
 
     @FXML
-    private void incrementCounter() {
-        counter++;
-        counterField.setText(String.valueOf(counter));
+    private TextField fField;
+
+    @FXML
+    private void convertFtoC() {
+        try {
+            double f = Double.parseDouble(fField.getText());
+            double c = Math.round((f - 32) / 1.8 * 100.0) / 100.0;
+            cField.setText(String.format("%.2f", c));
+        } catch (NumberFormatException e) {}
+    }
+
+    private void convertCtoF() {
+        try {
+            double c = Double.parseDouble(cField.getText());
+            double f = Math.round((1.8 * c + 32) * 100.0) / 100.0;
+            fField.setText(String.format("%.2f", f));
+        } catch (NumberFormatException e) {}
+    }
+
+    public void onCelsiusChange(KeyEvent keyEvent) {
+        convertCtoF();
+    }
+
+    public void onFahrenheitChange(KeyEvent keyEvent) {
+        convertFtoC();
     }
 }
+
+
--- src/sk/spse/uloha1/procedural/Application.java
+++ src/sk/spse/uloha1/procedural/Application.java
@@ -1,12 +1,13 @@
 package sk.spse.uloha1.procedural;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
 ///
 /// Trieda pre procedurálne vytvorené GUI
@@ -16,28 +17,54 @@ import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
 
-    private int counter = 0;
+    private TextField cField;
+    private TextField fField;
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Počítadlo:");
+        Label labelC = new Label("Stupne Celsia:");
+        cField = new TextField("0");
+        cField.setStyle("-fx-padding: 10 20 10 20;");
+        Label unitC = new Label("C");
 
-        TextField textField = new TextField("0");
+        HBox rowC = new HBox(10, labelC, cField, unitC);
+        rowC.setAlignment(Pos.CENTER_RIGHT);
+        rowC.setPadding(new Insets(10));
 
-        Button button = new Button("Plus 1");
+        Label labelF = new Label("Stupne Fahrenheita: ");
+        fField = new TextField("0");
+        fField.setStyle("-fx-padding: 10 20 10 20;");
+        Label unitF = new Label("F");
 
-        button.setOnAction(e -> {
-            counter++;
-            textField.setText(String.valueOf(counter));
-        });
+        HBox rowF = new HBox(10, labelF, fField, unitF);
+        rowF.setAlignment(Pos.CENTER_RIGHT);
+        rowF.setPadding(new Insets(10));
 
-        HBox root = new HBox(20, label, textField, button);
-        root.setPadding(new Insets(10));
+        VBox root = new VBox(0, rowC, rowF);
 
         Scene scene = new Scene(root);
+
+        cField.setOnKeyTyped(key -> convertCtoF());
+        fField.setOnKeyTyped(key -> convertFtoC());
 
         stage.setTitle("Procedural Application 1");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void convertCtoF() {
+        try {
+            double c = Double.parseDouble(cField.getText());
+            double f = 1.8 * c + 32;
+            fField.setText(String.format("%.2f", f));
+        } catch (NumberFormatException e) {}
+    }
+
+    private void convertFtoC() {
+        try {
+            double f = Double.parseDouble(fField.getText());
+            double c = (f - 32) / 1.8;
+            cField.setText(String.format("%.2f", c));
+        } catch (NumberFormatException e) {}
     }
 }
